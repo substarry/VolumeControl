70968d0e89a387771cea42fbc7fbeb4920856231
substarry <substarry@gmail.com>
2016-04-26 15:52:42 +0800

    init

---
+++ app/src/main/java/com/substarry/me/volumecontrol/CustomVolumControlBar.java
@@ -0,0 +1,246 @@
+package com.substarry.me.volumecontrol;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by 何凌 on 2016/4/26.
+ */
+public class CustomVolumControlBar extends View{
+
+    /**
+     * 第一圈的颜色
+     */
+    private int mFirstColor;
+
+    /**
+     * 第二圈的颜色
+     */
+    private int mSecondColor;
+    /**
+     * 圈的宽度
+     */
+    private int mCircleWidth;
+    /**
+     * 画笔
+     */
+    private Paint mPaint;
+    /**
+     * 当前进度
+     */
+    private int mCurrentCount = 3;
+
+    /**
+     * 中间的图片
+     */
+    private Bitmap mImage;
+    /**
+     * 每个块块间的间隙
+     */
+    private int mSplitSize;
+    /**
+     * 个数
+     */
+    private int mCount;
+
+    private Rect mRect;
+
+    public CustomVolumControlBar(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+
+    public CustomVolumControlBar(Context context)
+    {
+        this(context, null);
+    }
+
+    /**
+     * 必要的初始化，获得一些自定义的值
+     *
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
+    public CustomVolumControlBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        if (attrs != null) {
+            TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomVolumControlBar, defStyle, 0);
+            mFirstColor = a.getColor(R.styleable.CustomVolumControlBar_firstColor, Color.GREEN);
+            mSecondColor = a.getColor(R.styleable.CustomVolumControlBar_secondColor, Color.CYAN);
+            mImage = BitmapFactory.decodeResource(getResources(), a.getResourceId(R.styleable.CustomVolumControlBar_bg, 0));
+            mCircleWidth = a.getDimensionPixelSize(R.styleable.CustomVolumControlBar_circleWidth, (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics()));
+            mCount = a.getInt(R.styleable.CustomVolumControlBar_dotCount, 20);// 默认20
+            mSplitSize = a.getInt(R.styleable.CustomVolumControlBar_splitSize, 20);
+            a.recycle();
+        }
+
+        mPaint = new Paint();
+        mRect = new Rect();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+        int size;
+
+        if(widthMode == MeasureSpec.EXACTLY){
+            width = widthSize;
+        }
+        else{
+            //根据图片的宽度确定实际需要宽度
+            int imageWidth = mImage.getWidth();
+            width = (int)(imageWidth * Math.sqrt(2)) + mCircleWidth*2;
+        }
+
+        if(heightMode == MeasureSpec.EXACTLY){
+            height = heightSize;
+        }
+        else{
+            //根据图片的高度确定实际需要高度
+            int imageHeight = mImage.getHeight();
+            height = (int)(imageHeight * Math.sqrt(2)) + mCircleWidth*2;
+        }
+
+        //取宽度和高度中较大值，作为边长尺寸（正方形）
+        size = Math.max(width, height);
+        setMeasuredDimension(size, size);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mPaint.setAntiAlias(true); // 消除锯齿
+        mPaint.setStrokeWidth(mCircleWidth); // 设置圆环的宽度
+        mPaint.setStrokeCap(Paint.Cap.ROUND); // 定义线段断电形状为圆头
+        mPaint.setAntiAlias(true); // 消除锯齿
+        mPaint.setStyle(Paint.Style.STROKE); // 设置空心
+        int centre = getWidth() / 2; // 获取圆心的x坐标
+        int radius = centre - mCircleWidth / 2;// 半径
+        /**
+         * 画块块去
+         */
+        drawOval(canvas, centre, radius);
+
+        /**
+         * 计算内切正方形的位置
+         */
+        int relRadius = radius - mCircleWidth / 2;// 获得内圆的半径
+        /**
+         * 内切正方形的距离顶部 = mCircleWidth + relRadius - √2 / 2
+         */
+        mRect.left = (int) (relRadius - Math.sqrt(2) * 1.0f / 2 * relRadius) + mCircleWidth;
+        /**
+         * 内切正方形的距离左边 = mCircleWidth + relRadius - √2 / 2
+         */
+        mRect.top = (int) (relRadius - Math.sqrt(2) * 1.0f / 2 * relRadius) + mCircleWidth;
+        mRect.bottom = (int) (mRect.left + Math.sqrt(2) * relRadius);
+        mRect.right = (int) (mRect.left + Math.sqrt(2) * relRadius);
+
+        /**
+         * 如果图片比较小，那么根据图片的尺寸放置到正中心
+         */
+        if (mImage.getWidth() < Math.sqrt(2) * relRadius)
+        {
+            mRect.left = (int) (mRect.left + Math.sqrt(2) * relRadius * 1.0f / 2 - mImage.getWidth() * 1.0f / 2);
+            mRect.top = (int) (mRect.top + Math.sqrt(2) * relRadius * 1.0f / 2 - mImage.getHeight() * 1.0f / 2);
+            mRect.right = (int) (mRect.left + mImage.getWidth());
+            mRect.bottom = (int) (mRect.top + mImage.getHeight());
+
+        }
+        // 绘图
+        canvas.drawBitmap(mImage, null, mRect, mPaint);
+    }
+
+    /**
+     * 根据参数画出每个小块
+     *
+     * @param canvas
+     * @param centre
+     * @param radius
+     */
+    private void drawOval(Canvas canvas, int centre, int radius)
+    {
+        /**
+         * 根据需要画的个数以及间隙计算每个块块所占的比例*360
+         */
+        float itemSize = (360 * 1.0f - mCount * mSplitSize) / mCount;
+
+        RectF oval = new RectF(centre - radius, centre - radius, centre + radius, centre + radius); // 用于定义的圆弧的形状和大小的界限
+
+        mPaint.setColor(mFirstColor); // 设置圆环的颜色
+        for (int i = 0; i < mCount; i++)
+        {
+            canvas.drawArc(oval, i * (itemSize + mSplitSize), itemSize, false, mPaint); // 根据进度画圆弧
+        }
+
+        mPaint.setColor(mSecondColor); // 设置圆环的颜色
+        for (int i = 0; i < mCurrentCount; i++)
+        {
+            canvas.drawArc(oval, i * (itemSize + mSplitSize), itemSize, false, mPaint); // 根据进度画圆弧
+        }
+    }
+
+    /**
+     * 当前数量+1
+     */
+    public void up()
+    {
+        mCurrentCount++;
+        postInvalidate();
+    }
+
+    /**
+     * 当前数量-1
+     */
+    public void down()
+    {
+        mCurrentCount--;
+        postInvalidate();
+    }
+
+    private int xDown, xUp;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                xDown = (int) event.getY();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                xUp = (int) event.getY();
+                if (xUp > xDown)// 下滑
+                {
+                    down();
+                } else
+                {
+                    up();
+                }
+                break;
+        }
+
+        return true;
+    }
+}
